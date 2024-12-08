fc8183374121bb41590678e6661aec918bde4258
HRJ <hrj_jsut@163.com>
2024-12-08 23:17:30 +0800

    feat: 增加自定义数据集cuboid的训练脚本以及相关配置文件

---
+++ scripts/cuboid_train_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# export HYDRA_FULL_ERROR=1
+
+python -m src.main +experiment=cuboid \
+data_loader.train.batch_size=1 \
+dataset.test_chunk_interval=10 \
+trainer.val_check_interval=0.5 \
+trainer.max_steps=1000 \
+model.encoder.upsample_factor=4 \
+model.encoder.lowest_feature_resolution=4 \
+model.encoder.gaussian_regressor_channels=16 \
+model.encoder.feature_upsampler_channels=64 \
+model.encoder.return_depth=true \
+checkpointing.pretrained_monodepth=pretrained/depth_anything_v2_vits.pth \
+checkpointing.pretrained_mvdepth=pretrained/gmflow-scale1-things-e9887eda.pth \
+wandb.project=depthsplat \
+output_dir=checkpoints/cuboid-image4_test
