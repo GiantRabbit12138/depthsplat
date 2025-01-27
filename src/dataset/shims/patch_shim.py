a7ab4b049a32b0da92e117a44b27a56622780dd9
HRJ <hrj_jsut@163.com>
2025-01-27 15:34:31 +0800

    [dataset] feat: apply_patch_shim_to_view函数中增加对深度图的中心裁切处理

--- src/dataset/shims/patch_shim.py
+++ src/dataset/shims/patch_shim.py
@@ -2,7 +2,14 @@ from ..types import BatchedExample, BatchedViews
 
 
 def apply_patch_shim_to_views(views: BatchedViews, patch_size: int) -> BatchedViews:
+    """
+    该函数的主要作用是对批量图像（views["image"]）进行中心裁剪
+    使图像的尺寸对齐到给定的 patch_size 的倍数，同时根据裁剪调整相机内参矩阵（views["intrinsics"]）
+    """
     _, _, _, h, w = views["image"].shape
+    
+    # debug
+    # print("[apply_patch_shim_to_views] views[\"image\"].shape: {}".format(views["image"].shape))
 
     # Image size must be even so that naive center-cropping does not cause misalignment.
     assert h % 2 == 0 and w % 2 == 0
@@ -11,9 +18,14 @@ def apply_patch_shim_to_views(views: BatchedViews, patch_size: int) -> BatchedVi
     row = (h - h_new) // 2
     w_new = (w // patch_size) * patch_size
     col = (w - w_new) // 2
+    
+    # debug
+    # print("[apply_patch_shim_to_views] patch_size: {}".format(patch_size))
+    # print("[apply_patch_shim_to_views] h_new: {}, row: {}, w_new: {}, col: {}".format(h_new, row, w_new, col))
 
     # Center-crop the image.
     image = views["image"][:, :, :, row : row + h_new, col : col + w_new]
+    depth_image = views["depth_image"][:, :, :, row : row + h_new, col : col + w_new]
 
     # Adjust the intrinsics to account for the cropping.
     intrinsics = views["intrinsics"].clone()
@@ -23,6 +35,7 @@ def apply_patch_shim_to_views(views: BatchedViews, patch_size: int) -> BatchedVi
     return {
         **views,
         "image": image,
+        "depth_image": depth_image,
         "intrinsics": intrinsics,
     }
 
