66fa717a55b5793c34e59aa714e76b81ffd90edc
HRJ <hrj_jsut@163.com>
2025-01-27 16:37:38 +0800

    [model] style: 增加get_encoder的注释

--- src/model/encoder/__init__.py
+++ src/model/encoder/__init__.py
@@ -13,6 +13,9 @@ EncoderCfg = EncoderDepthSplatCfg
 
 
 def get_encoder(cfg: EncoderCfg) -> tuple[Encoder, Optional[EncoderVisualizer]]:
+    """
+    根据EncoderCfg类型的cfg配置文件，返回一个Encoder和EncoderVisualizer
+    """
     encoder, visualizer = ENCODERS[cfg.name]
     encoder = encoder(cfg)
     if visualizer is not None:
